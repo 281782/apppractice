6e10449d3284f6a84e897f2a5371329fbcd500f0
xiong <13772458726@139.com>
2021-01-27 13:49:11 +0800

    first commit

---
+++ src/main/java/com/example/zhuceyemian/ZhuceyemianApplication.java
@@ -0,0 +1,13 @@
+package com.example.zhuceyemian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhuceyemianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhuceyemianApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zhuceyemian/ZhuceyemianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zhuceyemian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhuceyemianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
